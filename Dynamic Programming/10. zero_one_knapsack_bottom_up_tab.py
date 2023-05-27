2df99be5f0d2805b7155d7358f68635d6bb9d95c
Andres Espinosa <andreseespinosao@gmail.com>
2023-05-27 00:53:14 -0500

    zero knapsack problem with dynamic prog

--- Dynamic Programming/10. zero_one_knapsack_bottom_up_tab.py
+++ Dynamic Programming/10. zero_one_knapsack_bottom_up_tab.py
@@ -1,27 +1,22 @@
-class Item:
-    def __init__(self, profit: int, weight: int) -> None:
-        self.profit = profit
-        self.weight = weight
-        
-def z0_knapsack(items: list, capacity: int, current_index: int, memo: dict = {}):
-    key = f"{current_index}{capacity}"
-    if capacity <= 0 or current_index < 0 or current_index >= len(items):
+
+def z0_knapsack(profits, weights, capacity):
+    if capacity <= 0 or len(profits) == 0 or len(weights) != len(profits):
         return 0
-    elif key in memo:
-        return memo[key]
-    elif items[current_index].weight <= capacity:
-        profit1 = items[current_index].profit + z0_knapsack(items, capacity-items[current_index].weight,current_index+1, memo)
-        profit2 = z0_knapsack(items, capacity,current_index+1, memo)
-        memo[key] = max(profit1, profit2)
-        return memo[key]
-    
-    return 0
+    numberOfRows = len(profits) + 1
+    dp = [[None for i in range(capacity+2)] for j in range(numberOfRows)]
+    for i in range(numberOfRows):
+        dp[i][0] = 0
+    for i in range(capacity+1):
+        dp[numberOfRows-1][i] = 0
+    for row in range(numberOfRows-2, -1, -1):
+        for column in range(1,capacity+1):
+            profit1 = 0
+            profit2 = 0
+            if weights[row] <= column:
+                profit1 = profits[row] + dp[row + 1][column - weights[row]]
+            profit2 = dp[row + 1][column]
+            dp[row][column] = max(profit1, profit2)
+    return dp[0][capacity]
 
-mango = Item(31,3)
-apple = Item(26,1)
-orange = Item(17,2)
-banana = Item(72,5)
 
-items = [mango, apple, orange, banana]
-
-print(z0_knapsack(items, 7, 0))
+print(z0_knapsack([], 7, 0))
--- Dynamic Programming/9. zero_one_knapsack_top_down_memo copy.py
+++ Dynamic Programming/9. zero_one_knapsack_top_down_memo copy.py
@@ -4,23 +4,18 @@ class Item:
         self.weight = weight
         
 def z0_knapsack(items: list, capacity: int, current_index: int, memo: dict = {}):
-    if capacity <= 0 or len(items) == 0 or len(capacity) != len(items):
+    key = f"{current_index}{capacity}"
+    if capacity <= 0 or current_index < 0 or current_index >= len(items):
         return 0
-    numberOfRows = len(items) + 1
-    dp = [[None for i in range(capacity+2)] for j in range(numberOfRows)]
-    for i in range(numberOfRows):
-        dp[i][0] = 0
-    for i in range(capacity+1):
-        dp[numberOfRows-1][i] = 0
-    for row in range(numberOfRows-2, -1, -1):
-        for column in range(1,capacity+1):
-            profit1 = 0
-            profit2 = 0
-            if capacity[row] <= column:
-                profit1 = items[row] + dp[row + 1][column - capacity[row]]
-            profit2 = dp[row + 1][column]
-            dp[row][column] = max(profit1, profit2)
-    return dp[0][capacity]
+    elif key in memo:
+        return memo[key]
+    elif items[current_index].weight <= capacity:
+        profit1 = items[current_index].profit + z0_knapsack(items, capacity-items[current_index].weight,current_index+1, memo)
+        profit2 = z0_knapsack(items, capacity,current_index+1, memo)
+        memo[key] = max(profit1, profit2)
+        return memo[key]
+    
+    return 0
 
 mango = Item(31,3)
 apple = Item(26,1)
