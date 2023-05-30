e9ace8e00ca764e4367d8abd1d959e616de74433
Andres Espinosa <andreseespinosao@gmail.com>
2023-05-29 23:29:15 -0500

    backtracking solve n queens

---
+++ Backtracking/1. n-queens.py
@@ -0,0 +1,59 @@
+class NQueens:
+    
+    def __init__(self, n) -> None:
+        self.n = n
+        self.board = [[0]*n]*n
+    
+    def print_queens(self):
+        for row in self.board:
+            for column in row:
+                if column == 1:
+                    print(' Q ', end='')
+                else:
+                    print(' _ ', end='')
+            print()
+            
+    def is_place_safe(self, row_index, col_index):
+        for column in self.board[row_index]:
+            if column == 1:
+                return False
+            
+        j = col_index
+        for i in range(row_index, -1, -1):
+            if i < 0:
+                break
+            if self.board[i][j] == 1:
+                return False
+            j = j-1
+        
+        j = col_index
+        for i in range(row_index, self.n):
+            if i < 0:
+                break
+            if self.board[i][j] == 1:
+                return False
+            j = j-1
+            
+        return True
+    
+    def solve(self, row_index=0):
+        if row_index == self.n:
+            return True
+        
+        for col_index in range(self.n):
+            if self.is_place_safe(row_index, col_index):
+                self.board[row_index][col_index] = 1
+                if self.solve(row_index+1):
+                    return True
+            self.board[row_index][col_index] = 0
+            
+        return False
+    
+    def solve_n_queens(self):
+        if self.solve():
+            self.print_queens()
+        else:
+            print('no solutions')
+    
+queens = NQueens(4)
+queens.solve_n_queens()
